5161ad188a355177b63aabd92844772dd69d0dd1
ThiagoLemoos <thiago.lemos.ps@gmail.com>
2024-05-19 16:22:24 -0300

    meu segundo commit

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0e001c9271cf7f3b9"
+  ami = "ami-0e001c9271cf7f3b9"
   instance_type = "t2.micro"
   key_name = "iac-alura"
   tags = {
-    Name = "Primeira Instancia"
+    Name = "Teste AWS"
   }
 }
